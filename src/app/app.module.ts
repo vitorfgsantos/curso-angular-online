0736460435d68cbb50998505a627a1f23954ae3c
Vitor Farias <vitorfgsantos@outlook.com>
2020-04-12 19:20:37 -0300

    feat: adicionado modulo shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,34 +9,22 @@ import { ExtratoComponent } from './extrato/extrato.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { NaoEncontradoComponent } from './nao-encontrado/nao-encontrado.component';
-import { ExercicioContadorComponent } from './shared/components/exercicio-contador/exercicio-contador.component';
-import { ExercicioDataBindingComponent } from './shared/components/exercicio-data-binding/exercicio-data-binding.component';
-import { ExercicioDiretivasComponent } from './shared/components/exercicio-diretivas/exercicio-diretivas.component';
-import { ExercicioNgclassComponent } from './shared/components/exercicio-ngclass/exercicio-ngclass.component';
-import { ExercicioPipesComponent } from './shared/components/exercicio-pipes/exercicio-pipes.component';
-import { FooterComponent } from './shared/components/footer/footer.component';
-import { HeaderComponent } from './shared/components/header/header.component';
+import { SharedModule } from './shared/shared.module';
 
 registerLocaleData(localePt, 'pt');
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     HomeComponent,
-    FooterComponent,
-    ExercicioDataBindingComponent,
-    ExercicioContadorComponent,
-    ExercicioDiretivasComponent,
-    ExercicioNgclassComponent,
-    ExercicioPipesComponent,
     LoginComponent,
     ExtratoComponent,
     NaoEncontradoComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
   ],
   providers: [{
     provide: LOCALE_ID,
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,35 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { ExercicioContadorComponent } from './components/exercicio-contador/exercicio-contador.component';
+import { ExercicioDataBindingComponent } from './components/exercicio-data-binding/exercicio-data-binding.component';
+import { ExercicioDiretivasComponent } from './components/exercicio-diretivas/exercicio-diretivas.component';
+import { ExercicioNgclassComponent } from './components/exercicio-ngclass/exercicio-ngclass.component';
+import { ExercicioPipesComponent } from './components/exercicio-pipes/exercicio-pipes.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    ExercicioDataBindingComponent,
+    ExercicioContadorComponent,
+    ExercicioDiretivasComponent,
+    ExercicioNgclassComponent,
+    ExercicioPipesComponent,
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent,
+    ExercicioDataBindingComponent,
+    ExercicioContadorComponent,
+    ExercicioDiretivasComponent,
+    ExercicioNgclassComponent,
+    ExercicioPipesComponent,
+  ],
+})
+export class SharedModule { }
