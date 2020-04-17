7d1eaf30f8e1b5de7b50b8ffe6f5edfdfa7c17c7
Vitor Farias <vitorfgsantos@outlook.com>
2020-04-17 01:49:23 -0300

    feat: adicionado botão delete e ngx-toastr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { HttpClientModule } from '@angular/common/http';
 import localePt from '@angular/common/locales/pt';
 import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NaoEncontradoComponent } from './nao-encontrado/nao-encontrado.component';
 import { SharedModule } from './shared/shared.module';
-
 
 registerLocaleData(localePt, 'pt');
 
@@ -19,9 +20,11 @@ registerLocaleData(localePt, 'pt');
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
     SharedModule,
+    ToastrModule.forRoot(),
   ],
   providers: [{
     provide: LOCALE_ID,
--- src/app/area-logada/contatos/listar-contatos/listar-contatos.component.ts
+++ src/app/area-logada/contatos/listar-contatos/listar-contatos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { finalize, take } from 'rxjs/operators';
 
 import { Contato } from '../contatos.interfaces';
@@ -19,6 +20,7 @@ export class ListarContatosComponent implements OnInit {
   constructor(
     private contatosService: ContatosService,
     private router: Router,
+    private toastr: ToastrService,
   ) { }
 
   ngOnInit() {
@@ -49,8 +51,26 @@ export class ListarContatosComponent implements OnInit {
     console.error(error);
   }
 
-  irParaDetalhes(idContato: string) {
+  irParaDetalhes(idContato: number) {
     this.router.navigate([`contatos/${idContato}`]);
+  }
+
+  deletarContato(idContato: number) {
+    this.contatosService.deleteContato(idContato.toString())
+      .subscribe(
+        response => this.onSuccessDeletarContato(idContato),
+        error => this.onErrorDeletarContato(),
+      );
+  }
+
+  onSuccessDeletarContato(idContato) {
+    this.toastr.success('Sucesso!', 'Contato deletado com sucesso.');
+    this.contatos = this.contatos.filter(contato => contato.id !== idContato);
+  }
+
+  onErrorDeletarContato() {
+    console.log('erro ao deletar!')
+    // faço alguma coisa
   }
 
 }
